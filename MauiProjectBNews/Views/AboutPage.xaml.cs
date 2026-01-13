ce907eef543e28d8b43ae560b8289b00e11a5438
Erik Forsgren <crapmannen@hotmail.com>
2026-01-13 13:22:07 +0100

    Fixed AboutPage to show up in the menu.

--- MauiProjectBNews/Views/AboutPage.xaml.cs
+++ MauiProjectBNews/Views/AboutPage.xaml.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿
+using MauiProjectBNews.Models;
+using MauiProjectBNews.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiProjectBNews.Views
 {
-    internal class AboutPage
+    public partial class AboutPage : ContentPage
     {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+        }
     }
 }
